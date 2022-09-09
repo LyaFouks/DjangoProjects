f07f602f2bed6ade88462af6abd55b5b27066e87
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-09-09 10:30:36 +0300

    Templates

--- myFirstProjectRoot/myFirstProject/views.py
+++ myFirstProjectRoot/myFirstProject/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def about(request):
 	return HttpResponse('This is about')
 
 
 def home(request):
-	return HttpResponse('<h1>This is home</h1>')
+	return HttpResponse(request, 'home.html', {'greeting':'Hello!'})
